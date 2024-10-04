79df9aefa13ec3fa27e2abd352ef5d40d6914e0f
wiechuzw <wiechuzw@users.noreply.github.com>
2024-10-04 15:58:58 +0000

    deploy: 982b1346753167053c2fbfe6ffd425e4f06099d0

---
+++ data_plot.py
@@ -0,0 +1,163 @@
+import os
+from typing import Any
+import locale
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.lines import Line2D
+from sunrise_sunset import get_daylight_hours
+import numpy as np
+import seaborn as sns
+import pandas as pd
+
+FILE = './data_weather/visualcrossing.csv'
+CONFIG_FILE = 'Config_file.toml'
+
+def modify_loc(location: str) -> str:
+    '''
+    Shortens the name of location to only city name
+    '''
+    return location.split(',')[0]
+
+def night_hours(data: pd.DataFrame, ax1: Any, ax2: Any) -> None:
+    ''' Marks hours between 22 PM and 6 AM as 'night hours'. '''
+    sunrise, sunset = get_daylight_hours(CONFIG_FILE)
+    sunrise = sunrise.split(':')
+    sunset = sunset.split(':')
+    for i in range(len(data.index) - 1):
+        current_time = data.index[i]
+        next_time = data.index[i + 1]
+
+        if (current_time.hour >= int(sunset[0]) or current_time.hour < int(sunrise[0])):
+            ax1.axvspan(current_time, next_time, facecolor='#333333', alpha=0.2)
+            ax2.axvspan(current_time, next_time, facecolor='#333333', alpha=0.2)
+
+def get_data(hours: int) -> pd.DataFrame:
+    '''
+    Loads data from specified *.csv file for given number of hours. 
+    Returns 'DataFrame' object.
+    '''
+    columns = ['name', 'datetime', 'temp', 'feelslike', 'winddir','windspeed', 
+            'windgust', 'precip', 'snow', 'cloudcover' ]
+    with open(FILE, encoding='utf-8') as reader:
+        data = pd.read_csv(reader, 
+                        usecols=columns,
+                        index_col='datetime',
+                        parse_dates=True                       
+                        )
+
+    data['name'] = data['name'].apply(modify_loc)
+    data = data.iloc[12:hours+12]
+    return data
+
+
+def main():
+    data = get_data(56)
+
+    # Try setting locale to Polish, fallback to English if it fails
+    try:
+        locale.setlocale(locale.LC_TIME, 'polish')
+    except locale.Error:
+        locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')  # Fallback to English locale
+
+    fig, ax = plt.subplots(2, 1, figsize=(15,6), layout='constrained')
+    sns.set_style("whitegrid")
+
+    # TOP Graph
+
+    # Wind and wind gusts
+    ax_left_up = ax[0]
+    wind = ax_left_up.bar(data.index, data['windspeed'], width=0.04, color='green', label='Prędkość wiatru')
+    gust = ax_left_up.scatter(data.index, data['windgust'], c='steelblue', marker='_', s=100, label='Porywy wiatru')
+
+    ax_left_up.set_title(f'Prognoza na następne 48h dla {data["name"].iloc[0]}')
+    ax_left_up.set_ylabel('Prędkość wiatru (km/h)')
+
+    # Set temp readout on one graph
+    ax_right_up = ax_left_up.twinx()
+    ax_right_up.plot(data.index, data['temp'], color='darkred', label="Temperatura")
+    ax_right_up.plot(data.index, data['feelslike'], color='pink', label='Temperatura odczuwalna')
+    ax_right_up.set_ylabel('Temperatura (°C)', color='red')
+    ax_right_up.tick_params(axis='y', labelcolor='red')
+
+    ax[0].xaxis.set_major_locator(mdates.HourLocator(interval=2))  
+    ax[0].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))  
+    plt.setp(ax[0].xaxis.get_majorticklabels(), rotation=45, ha='right')
+    # Set date as larger 'tic'
+    ax[0].xaxis.set_minor_locator(mdates.DayLocator(interval=1))  
+    ax[0].xaxis.set_minor_formatter(mdates.DateFormatter('%d-%B'))  
+    ax[0].xaxis.set_minor_locator(mdates.HourLocator(byhour=12))
+    ax[0].tick_params(axis='x', which='minor', pad=30, labelsize='large')  
+
+    # Grid
+    ax[0].grid(which='major', linestyle='-', linewidth='0.5', color='gray')
+    ax[0].grid(which='minor', linestyle=':', linewidth='0.5', color='gray')
+
+    # Filling color between 'temp' and 'feelslike'
+    ax_right_up.fill_between(data.index, data['temp'], data['feelslike'], 
+                            where=(data['temp'] != data['feelslike']),
+                            color='salmon', alpha=0.2)
+
+    # Arrows pointing wind direction
+    arrow_length = 1.0
+    arrow_y = data['temp'].mean()
+    for i in range(len(data)):
+        wind_dir_rad = np.deg2rad(data['winddir'].iloc[i])  
+        x = data.index[i] 
+       
+        dx = arrow_length * np.cos(wind_dir_rad)  
+        dy = arrow_length * np.sin(wind_dir_rad) 
+
+        dx_timedelta = pd.Timedelta(hours=dx)
+        
+        ax_right_up.annotate('', xy=(x + dx_timedelta, arrow_y + dy), xytext=(x, arrow_y),
+                            arrowprops=dict(arrowstyle='->', color='red', lw=1.5))
+
+    # Lower Graph
+    ax_left_dwn = ax[1]
+    ax_left_dwn.bar(data.index, data['cloudcover'], width=0.04, color='lightblue', label="Chmury")  
+    ax_left_dwn.set_title('Zachmurzenie')
+    ax_left_dwn.set_ylabel('Pokrywa chmur (%)')
+
+    ax_right_dwn = ax_left_dwn.twinx()
+    ax_right_dwn.bar(data.index, data['precip'], width=0.02, color='blue', label='Opady deszczu')
+    ax_right_dwn.bar(data.index, data['snow'], width=0.02, color='red', label='Opady śniegu')
+
+    ax_right_dwn.set_ylabel('Opady (mm)', color='gray')
+    ax_right_dwn.set_ylim(bottom=0)
+    ax_right_dwn.tick_params(axis='y', labelcolor='black')
+
+    # Setting axis labels for the lower graph
+    ax_left_dwn.xaxis.set_major_locator(mdates.HourLocator(interval=2))  
+    ax_left_dwn.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))  
+    plt.setp(ax_left_dwn.xaxis.get_majorticklabels(), rotation=45, ha='right')
+    # Set date as larger 'tic'
+    ax_left_dwn.xaxis.set_minor_locator(mdates.DayLocator(interval=1))  
+    ax_left_dwn.xaxis.set_minor_formatter(mdates.DateFormatter('%d-%B'))  
+    ax_left_dwn.xaxis.set_minor_locator(mdates.HourLocator(byhour=12))
+    ax_left_dwn.tick_params(axis='x', which='minor', pad=30, labelsize='large')  
+    # Grid
+    ax_left_dwn.grid(which='major', linestyle='-', linewidth='0.5', color='blue')
+    ax_right_dwn.grid(which='minor', linestyle=':', linewidth='0.5', color='orange')
+    # Created 'Wind Direction Marker' 
+    arrow_legend = Line2D([0], [0], color='red', lw=0.5, marker='>', markersize=5, label='Kierunek wiatru')
+
+    # Legends
+    ax[0].legend(handles=(wind, gust, arrow_legend), loc='upper left')
+    ax_right_up.legend(loc='upper right')
+    ax_left_dwn.legend(loc='upper left')
+    ax_right_dwn.legend(loc='upper right')
+
+    night_hours(data, ax[0], ax[1])
+
+    output_file = os.path.join(os.getcwd(), 'weather_plot.png')
+    
+    try:
+        plt.savefig(output_file)
+        print(f'Plot saved successfully as {output_file}')
+    except Exception as e:
+        print(f'Error saving plot: {e}')
+
+if __name__ == "__main__":
+    main()
+    plt.savefig('weather_plot.png')
+    plt.savefig(os.path.join('website', 'weather_plot2.png'))
---
+++ sunrise_sunset.py
@@ -0,0 +1,66 @@
+import requests
+import toml
+from datetime import datetime, timedelta
+
+import pytz  # Import pytz for timezone handling
+
+
+def read_coordinates(file: str):
+    """
+    Reads the latitude and longitude from a configuration file in TOML format.
+    """
+    config = toml.load(file)
+    return config['latitude'], config['longitude']
+
+def get_daylight_hours(file: str, date: str = None):
+    """
+
+    Finds the sunrise and sunset times for a given date and rounds them to full hours with half-hour accuracy.
+    If the date is not given, the data for the current day is provided.
+    It finds sunrise and sunset time for a specific location according to the configuration file.
+    The latitude and longitude (specific location) are loaded from a configuration file in TOML format.
+    """
+    lat, lon = read_coordinates(file)
+    
+    if date is None:
+        date = datetime.now().strftime('%Y-%m-%d')
+
+    url = f"https://api.sunrise-sunset.org/json?lat={lat}&lng={lon}&date={date}&formatted=0"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Ensure a successful response
+        data = response.json()
+        
+        # Extract sunrise and sunset times in UTC
+        sunrise_utc = data['results']['sunrise']
+        sunset_utc = data['results']['sunset']
+        
+        # Convert from UTC to local time (Europe/Warsaw)
+        local_tz = pytz.timezone('Europe/Warsaw')
+        sunrise = datetime.fromisoformat(sunrise_utc).astimezone(local_tz)
+        sunset = datetime.fromisoformat(sunset_utc).astimezone(local_tz)
+        
+        # Rounding to whole hours
+        sunrise = (sunrise + timedelta(minutes=30)).replace(minute=0, second=0, microsecond=0)
+        sunset = (sunset + timedelta(minutes=30)).replace(minute=0, second=0, microsecond=0)
+
+        return sunrise.strftime('%H:%M'), sunset.strftime('%H:%M')
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching sunrise and sunset data: {e}")
+        return None, None
+        
+
+def main():
+    config_file = 'Config_file.toml'
+    sunrise, sunset = get_daylight_hours(config_file)
+
+    if sunrise and sunset:
+        print(f"Sunrise: {sunrise}, Sunset: {sunset}")
+    else:
+        print("Failed to fetch sunrise and sunset times.")
+        
+
+if __name__ == '__main__':
+    main()
